9d5b95518beeb608e8af91ea121c220e7862e83a
Ben Hunter <code@benhunter.me>
2021-02-12 07:51:43 -0800

    Day 15 in progress

--- AdventOfCode/2020/day15.py
+++ AdventOfCode/2020/day15.py
@@ -2,10 +2,31 @@
 # https://adventofcode.com/2020/day/15
 
 
+TARGET = 2020
+
+
 if __name__ == "__main__":
-    filename = "./AdventOfCode/2020/day15-input.txt"
-    # filename = "./AdventOfCode/2020/day15-example1-input.txt"
+    # filename = "./AdventOfCode/2020/day15-input.txt"
+    filename = "./AdventOfCode/2020/day15-example1-input.txt"
 
     with open(filename) as f:
         lines = [line.rstrip() for line in f]
     print(lines)
+
+    # numbers = [int(x) for (x:=line.split(",")) for line in lines]
+    seed_nums = [int(x) for line in lines for x in line.split(",")]
+    print(seed_nums)
+
+    nums = seed_nums.copy()
+    print(nums)
+
+    for i in range(TARGET):
+        if len(nums) > i:
+            continue
+        # print(i)
+
+        last_num = nums[-1]
+
+        diff = len(nums) - prev_position  # or 0 if not in nums
+
+        nums.append(diff)
