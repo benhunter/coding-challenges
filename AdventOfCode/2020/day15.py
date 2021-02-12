fa637f41370d6d15ad74edd0eb620ae1c4f75fda
Ben Hunter <code@benhunter.me>
2021-02-12 07:20:21 -0800

    Starting AOC 2020 Day 15

---
+++ AdventOfCode/2020/day15.py
@@ -0,0 +1,11 @@
+# Advent of Code Day 15
+# https://adventofcode.com/2020/day/15
+
+
+if __name__ == "__main__":
+    filename = "./AdventOfCode/2020/day15-input.txt"
+    # filename = "./AdventOfCode/2020/day15-example1-input.txt"
+
+    with open(filename) as f:
+        lines = [line.rstrip() for line in f]
+    print(lines)
