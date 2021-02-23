14164de41a13de32af5f660df0e5efc765fd7c6b
Ben Hunter <code@benhunter.me>
2021-02-22 20:53:32 -0800

    Advent of Code 2020 Day 18 Solved

--- AdventOfCode/2020/day18.py
+++ AdventOfCode/2020/day18.py
@@ -6,6 +6,7 @@ from collections import namedtuple
 from typing import List
 
 
+# Part 1
 # Grammar:
 # Expression =  "(" Expression ")" |
 #               Expression Operator Number |
@@ -47,29 +48,84 @@ def parse(tokens: List[Token]):
     if len(tokens) == 0:
         return None
     if len(tokens) == 1:
+        if type(tokens[0]) == int:
+            return tokens[0]
+        elif tokens[0].type == "Number":
+            return tokens[0].value
+
+    if tokens[-2].type == "Operator":
+        operand2 = tokens[-1] if type(tokens[-1]) is int else tokens[-1].value
+        if tokens[-2].value == "Add":
+            return parse(tokens[:-2]) + operand2
+        elif tokens[-2].value == "Multiply":
+            return parse(tokens[:-2]) * operand2
+
+    if tokens[-1].type == "CloseParen":
+        paren_count = 1
+        index = len(tokens) - 1
+        while paren_count != 0:
+            index -= 1
+            if tokens[index].type == "OpenParen":
+                paren_count -= 1
+            elif tokens[index].type == "CloseParen":
+                paren_count += 1
+        return parse(tokens[:index] + [parse(tokens[index + 1 : -1])])
+
+
+# Part 1
+# Grammar:
+# Expression =  "(" Expression ")" |
+#               Expression Multiply Number |
+#               Number
+# Multiply =    Expression "*" Expression |
+#               Add "*" Expression
+# Add =         Expression "+" Expression
+# Number =      int()
+
+# if parens, parse those
+# if adds, parse those
+# if muls, parse those
+
+
+def parse_part2(tokens: List[Token]):
+    # Recursive
+    # Base case
+    if type(tokens) is int:
+        return tokens
+    if len(tokens) == 0:
+        raise RuntimeError()
+    if len(tokens) == 1:
+        if type(tokens[0]) is int:
+            return tokens[0]
         if tokens[0].type == "Number":
             return tokens[0].value
 
-    if tokens[0].type == "OpenParen":
-        # TODO wrong close parens ex: ()()
-        # index_close_paren = reversed().index(Token("CloseParen"))
-        return parse(tokens[1:index_close_paren])
+    if Token("OpenParen") in tokens:
+        open_paren_index = tokens.index(Token("OpenParen"))
+        paren_count = 1
+        close_paren_index = open_paren_index
+        while paren_count != 0:
+            close_paren_index += 1
+            if tokens[close_paren_index].type == "OpenParen":
+                paren_count += 1
+            elif tokens[close_paren_index].type == "CloseParen":
+                paren_count -= 1
+        return parse_part2(
+            tokens[:open_paren_index]
+            + [parse_part2(tokens[open_paren_index + 1 : close_paren_index])]
+            + tokens[close_paren_index + 1 :]
+        )
 
-    if tokens[-2].type == "Operator":
-        if tokens[-2].value == "Add":
-            return parse(tokens[:-2]) + tokens[-1].value
-        elif tokens[-2].value == "Multiply":
-            return parse(tokens[:-2]) * tokens[-1].value
-
-
-    # old
-    # if tokens[1].type == "Operator":
-    #     if tokens[1].value == "Add":
-    #         # TODO fix L->R parsing, order of operations
-    #         return tokens[0].value + parse(tokens[2:])
-    #     elif tokens[1].value == "Multiply":
-    #         # TODO same as above
-    #         return parse(tokens[2:] * tokens[0].value)
+    elif Token("Operator", "Multiply") in tokens:
+        mul_index = tokens.index(Token("Operator", "Multiply"))
+        return parse_part2(
+            parse_part2(tokens[:mul_index]) * parse_part2(tokens[mul_index + 1 :])
+        )
+    elif Token("Operator", "Add") in tokens:
+        add_index = tokens.index(Token("Operator", "Add"))
+        return parse_part2(
+            parse_part2(tokens[:add_index]) + parse_part2(tokens[add_index + 1 :])
+        )
 
 
 def test_tokenize():
@@ -100,7 +156,14 @@ if __name__ == "__main__":
     # print(lines)
     # for line in lines:
     # print(line)
-    print(lines[0])
-    print(tokenize(lines[0]))
-    print(parse(tokenize(lines[0])))
-    print(tokenize(lines[1]))
+    # print(lines[0])
+    # print(tokenize(lines[0]))
+    # print(parse(tokenize(lines[0])))
+    # print(tokenize(lines[1]))
+    # print(parse(tokenize(lines[1])))
+
+    part1 = sum([parse(tokenize(line)) for line in lines])
+    print(f"Part 1: {part1}")  # 209335026987
+
+    part2 = sum([parse_part2(tokenize(line)) for line in lines])
+    print(f"Part 2: {part2}")  # 33331817392479
