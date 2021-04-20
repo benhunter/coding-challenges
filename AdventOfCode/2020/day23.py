2753c5ad5afacc9e09fc5e4fcd43ae036fd4e6cb
Ben Hunter <code@benhunter.me>
2021-04-20 09:35:44 -0700

    Dict based circular linked-list is passing first example case

--- AdventOfCode/2020/day23.py
+++ AdventOfCode/2020/day23.py
@@ -82,13 +82,9 @@ def select_destination(cups, current_value, next_three):
 
 
 def move(cups, moves):
-    if PROFILE:
-        inner_loop_count = 0  # profiling helper
     current_posn = 0
     current_value = cups[current_posn]
-    # three_cups = next_three(cups, current_posn)
     len_cups = len(cups)
-
     move_count = 1
     for _ in range(moves):
         if PROFILE and move_count % 1000 == 0:
@@ -96,48 +92,59 @@ def move(cups, moves):
         if DEBUG:
             print(f"-- move {move_count} --")
             print(f"cups: {repr_cups(cups, current_posn)}")
-
         index = current_posn + 1
         next_three = []
         if index + 3 < len_cups:
-            next_three = cups[index:index + 3]
-            del cups[index:index + 3]
+            next_three = cups[index : index + 3]
+            del cups[index : index + 3]
         else:
             for i in range(3):
                 if index >= len(cups):
                     index = 0
                 next_three.append(cups.pop(index))
-
         if DEBUG:
             print(f"pick up: {', '.join(str(cup) for cup in next_three)}")
-
         destination_posn = select_destination(cups, current_value, next_three)
-
-        # place next_three into cups after destination_posn
-        # option 1
-        # cups = cups[:destination_posn + 1] + next_three + cups[destination_posn + 1:]
-        # option 2
-        # cups = [*cups[:destination_posn + 1], *next_three, *cups[destination_posn + 1:]]
-        # option 3
-        # new_cups = cups[:destination_posn + 1]
-        # new_cups += next_three
-        # new_cups += cups[destination_posn + 1:]
-        # cups = new_cups
-        # option 4
-        # cups.insert(destination_posn + 1, next_three)  # does not unpack next_three
-        cups[destination_posn + 1:destination_posn + 1] = next_three
-
+        cups[destination_posn + 1 : destination_posn + 1] = next_three
         move_count += 1
-            
         if current_posn > destination_posn:
-            # will change if next_three 
+            # will change if next_three
             # was inserted before current_posn
             current_posn += 3
-
         current_posn = (current_posn + 1) if (current_posn + 1) < len_cups else 0
         current_value = cups[current_posn]
-
     return cups
+
+
+def move_dict(cups, moves, current):
+    # cups list to dict
+    cups_dict = {}
+    for index, cup in enumerate(cups[:-1]):
+        cups_dict[cup] = cups[index + 1]
+    cups_dict[cups[-1]] = cups[0]
+
+    for _ in range(moves):
+        current_next = cups_dict[cups_dict[cups_dict[cups_dict[current]]]]
+        next_three = [
+            cups_dict[current],
+            cups_dict[cups_dict[current]],
+            cups_dict[cups_dict[cups_dict[current]]]
+        ]
+        # check for negative dest
+        # and dest not in next_three
+        dest = current - 1
+        while dest in next_three or dest < 1:
+            if dest < 1:
+                dest = max(cups_dict.keys())
+            else:
+                dest = dest - 1
+        dest_next = cups_dict[dest]
+
+        cups_dict[dest] = cups_dict[current]
+        cups_dict[cups_dict[cups_dict[cups_dict[dest]]]] = dest_next
+        cups_dict[current] = current_next
+        current = current_next
+    return cups_dict
 
 
 def test_move_part1_example1_10():
@@ -145,7 +152,22 @@ def test_move_part1_example1_10():
     cups = load_cups(filename)
     cups = move(cups, 10)
     one_index = cups.index(1)
-    part1 = "".join(str(cup) for cup in cups[one_index + 1:] + cups[:one_index])
+    part1 = "".join(str(cup) for cup in cups[one_index + 1 :] + cups[:one_index])
+    assert part1 == "92658374"
+
+
+def test_move_part1_example1_10_dict():
+    filename = "./AdventOfCode/2020/day23-example1-input.txt"
+    cups = load_cups(filename)
+    # cups = move(cups, 10)
+    cups = move_dict(cups, 10, cups[0])
+    # one_index = cups[1]
+    # part1 = "".join(str(cup) for cup in cups[one_index + 1 :] + cups[:one_index])
+    part1 = ""
+    current = 1
+    for _ in range(len(cups.keys()) - 1):
+        part1 += str(cups[current])
+        current = cups[current]
     assert part1 == "92658374"
 
 
@@ -154,7 +176,7 @@ def test_move_part1_example1_100():
     cups = load_cups(filename)
     cups = move(cups, 100)
     one_index = cups.index(1)
-    part1 = "".join(str(cup) for cup in cups[one_index + 1:] + cups[:one_index])
+    part1 = "".join(str(cup) for cup in cups[one_index + 1 :] + cups[:one_index])
     assert part1 == "67384529"
 
 
@@ -163,15 +185,15 @@ def test_move_part1_input_100():
     cups = load_cups(filename)
     cups = move(cups, 100)
     one_index = cups.index(1)
-    part1 = "".join(str(cup) for cup in cups[one_index + 1:] + cups[:one_index])
+    part1 = "".join(str(cup) for cup in cups[one_index + 1 :] + cups[:one_index])
     assert part1 == "28793654"
 
 
 def test_part2_example1():
     filename = "./AdventOfCode/2020/day23-example1-input.txt"
     cups = load_cups(filename)
-    cups += list(range(max(cups) + 1,1000000))
-    cups = move(cups, 10000000)
+    cups += list(range(max(cups) + 1, 1000000))
+    # cups = move(cups, 10000000)
     one_index = cups.index(1)
     assert cups[one_index + 1] == 934001
     assert cups[one_index + 2] == 159792
@@ -183,7 +205,7 @@ def profile_part2():
     MOVES = 10000
     filename = "./AdventOfCode/2020/day23-example1-input.txt"
     cups = load_cups(filename)
-    cups += list(range(max(cups) + 1,1000000))
+    cups += list(range(max(cups) + 1, 1000000))
     with cProfile.Profile() as pr:
         cups = move(cups, MOVES)
     pr.print_stats()
@@ -202,7 +224,7 @@ def main():
     print(f"cups: {repr_cups(cups, 0)}")
 
     one_index = cups.index(1)
-    part1 = "".join(str(cup) for cup in cups[one_index + 1:] + cups[:one_index])
+    part1 = "".join(str(cup) for cup in cups[one_index + 1 :] + cups[:one_index])
     print(f"Part 1: {part1}")  # 28793654
 
     # Part 2
@@ -212,7 +234,9 @@ def main():
 
 
 if __name__ == "__main__":
-    if PROFILE:
-        profile_part2()
+    # if PROFILE:
+    #     profile_part2()
     # test_part2_example1()
     # main()
+
+    test_move_part1_example1_10_dict()
