3067194ba27daf94225dd9206eebe1a5d116e7f2
Ben Hunter <code@benhunter.me>
2021-04-22 22:03:40 -0700

    refactor

--- AdventOfCode/2020/day23.py
+++ AdventOfCode/2020/day23.py
@@ -3,18 +3,24 @@
 
 
 import cProfile
-from typing import List
+from typing import Dict, List
 
 DEBUG = False
 PROFILE = True
 
 
-def load_cups(filename):
+def load_cups(filename: str):
     with open(filename) as f:
         lines = [line.rstrip() for line in f]
     cups = lines[0]
     cups = [int(cup) for cup in cups]
-    return cups
+
+    # cups list to dict
+    cups_dict = {}
+    for index, cup in enumerate(cups[:-1]):
+        cups_dict[cup] = cups[index + 1]
+    cups_dict[cups[-1]] = cups[0]
+    return cups_dict
 
 
 def repr_cups(cups: List, current_posn):
@@ -37,39 +43,34 @@ def test_repr_cups():
     assert repr_cups(cups, current_posn) == " 0  1 (2) 3 "
 
 
-def move(cups, moves):
-    # cups list to dict
-    cups_dict = {}
-    for index, cup in enumerate(cups[:-1]):
-        cups_dict[cup] = cups[index + 1]
-    cups_dict[cups[-1]] = cups[0]
-    current = cups[0]
+def move(cups: Dict, moves):
+    current = next(iter(cups.keys()))
 
     for _ in range(moves):
-        current_next = cups_dict[cups_dict[cups_dict[cups_dict[current]]]]
+        current_next = cups[cups[cups[cups[current]]]]
         next_three = [
-            cups_dict[current],
-            cups_dict[cups_dict[current]],
-            cups_dict[cups_dict[cups_dict[current]]],
+            cups[current],
+            cups[cups[current]],
+            cups[cups[cups[current]]],
         ]
         # check for negative dest
         # and dest not in next_three
         dest = current - 1
         while dest in next_three or dest < 1:
             if dest < 1:
-                dest = max(cups_dict.keys())
+                dest = max(cups.keys())
             else:
                 dest = dest - 1
-        dest_next = cups_dict[dest]
+        dest_next = cups[dest]
 
-        cups_dict[dest] = cups_dict[current]
-        cups_dict[cups_dict[cups_dict[cups_dict[dest]]]] = dest_next
-        cups_dict[current] = current_next
+        cups[dest] = cups[current]
+        cups[cups[cups[cups[dest]]]] = dest_next
+        cups[current] = current_next
         current = current_next
-    return cups_dict
+    return cups
 
 
-def test_move_part1_example1_10_dict():
+def test_move_part1_example1_10():
     filename = "./AdventOfCode/2020/day23-example1-input.txt"
     cups = load_cups(filename)
     cups = move(cups, 10)
@@ -81,7 +82,7 @@ def test_move_part1_example1_10_dict():
     assert part1 == "92658374"
 
 
-def test_move_part1_example1_100_dict():
+def test_move_part1_example1_100():
     filename = "./AdventOfCode/2020/day23-example1-input.txt"
     cups = load_cups(filename)
     cups = move(cups, 100)
@@ -93,7 +94,7 @@ def test_move_part1_example1_100_dict():
     assert part1 == "67384529"
 
 
-def test_move_part1_input_100_dict():
+def test_move_part1_input_100():
     filename = "./AdventOfCode/2020/day23-input.txt"
     cups = load_cups(filename)
     cups = move(cups, 100)
@@ -105,10 +106,17 @@ def test_move_part1_input_100_dict():
     assert part1 == "28793654"
 
 
-def test_part2_example1_dict():
+def test_part2_example1():
     filename = "./AdventOfCode/2020/day23-example1-input.txt"
     cups = load_cups(filename)
-    cups += list(range(max(cups) + 1, 1000000 + 1))
+    first = list(cups.keys())[0]
+    last = list(cups.keys())[-1]
+    prev = last
+    for i in range(max(cups) + 1, 1000000 + 1):
+        cups[prev] = i
+        prev = i
+    cups[prev] = first
+
     assert len(cups) == 1000000
     cups = move(cups, 10000000)
     assert cups[1] == 934001
@@ -149,14 +157,23 @@ def main():
     print(f"-- final --")
     print(f"cups: {repr_cups(cups, 0)}")
 
-    one_index = cups.index(1)
-    part1 = "".join(str(cup) for cup in cups[one_index + 1 :] + cups[:one_index])
+    part1 = ""
+    current = 1
+    for _ in range(len(cups) - 1):
+        part1 += str(cups[current])
+        current = cups[current]
     print(f"Part 1: {part1}")  # 28793654
     assert part1 == "28793654"
 
     # Part 2
     cups = orig_cups.copy()
-    cups += list(range(max(cups) + 1, 1000000 + 1))
+    first = list(cups.keys())[0]
+    last = list(cups.keys())[-1]
+    prev = last
+    for i in range(max(cups) + 1, 1000000 + 1):
+        cups[prev] = i
+        prev = i
+    cups[prev] = first
     cups = move(cups, 10000000)
     part2 = cups[1] * cups[cups[1]]
     print(f"Part 2: {part2}")  # 359206768694
@@ -164,9 +181,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # if PROFILE:
-    #     profile_part2()
-    #     profile_part2_dict()
-    # test_part2_example1()
-    # test_move_part1_example1_10_dict()
     main()
