62f843b349049b373b263b2621f81b462d88aeec
Ben Hunter <code@benhunter.me>
2021-12-02 07:15:09 -0600

    AOC 2021 day 2

--- AdventOfCode/2021/day2.py
+++ AdventOfCode/2021/day2.py
@@ -3,30 +3,30 @@ with open('day2-input.txt') as f:
     data = list(map(lambda command: [command[0], int(command[1])], data))
     # print(data)
 
-horzontal_position = 0
+horizontal_position = 0
 depth = 0
 
 for direction, distance in data:
     if direction == "forward":
-        horzontal_position += distance
+        horizontal_position += distance
     elif direction == "down":
         depth += distance
     elif direction == "up":
         depth -= distance
 
-print(f'Part 1: {horzontal_position * depth}')
+print(f'Part 1: {horizontal_position * depth}')
 
-horzontal_position = 0
+horizontal_position = 0
 depth = 0
 aim = 0
 
 for direction, distance in data:
     if direction == "forward":
-        horzontal_position += distance
+        horizontal_position += distance
         depth += aim * distance
     elif direction == "down":
         aim += distance
     elif direction == "up":
         aim -= distance
 
-print(f'Part 2: {horzontal_position * depth}')
+print(f'Part 2: {horizontal_position * depth}')
