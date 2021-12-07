1a8b58303a6008ecfb59ccd2cbe384fedd1812dd
Ben Hunter <code@benhunter.me>
2021-12-07 07:43:16 -0600

    Refactor - removed list brackets, added int() around division.

--- AdventOfCode/2021/7.py
+++ AdventOfCode/2021/7.py
@@ -4,11 +4,11 @@ with open('7-input.txt') as f:
 # with open('7-input-test.txt') as f:
     data = f.readline().strip().split(',')
     data = list(map(int, data))
-    print(data)
+    # print(data)
 
-part1 = min([sum(abs(posn - x) for x in data) for posn in range(min(data), max(data) + 1)])
+part1 = min(sum(abs(posn - x) for x in data) for posn in range(min(data), max(data) + 1))
 print(f'Part 1: {part1}')  # Part 1: 349769
 
 # Part 2
-part2 = min([sum(abs((posn - x) * (1 + abs(posn - x)) / 2) for x in data) for posn in range(min(data), max(data) + 1)])
+part2 = min(sum(int(abs((posn - x) * (1 + abs(posn - x)) / 2)) for x in data) for posn in range(min(data), max(data) + 1))
 print(f'Part 2: {part2}')  # Part 2: 99540554
