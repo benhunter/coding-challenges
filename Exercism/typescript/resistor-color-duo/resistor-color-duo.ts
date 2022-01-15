7550713f1565f5a22b8f6677dbe31f0132c0d7a1
Ben Hunter <code@benhunter.me>
2022-01-15 15:38:18 -0600

    Add Exercism Typescript Resistor-color-duo

--- Exercism/typescript/resistor-color-duo/resistor-color-duo.ts
+++ Exercism/typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,23 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(colors: string[]): number {
+    let colorMap = new Map<string, number>([
+            ['black', 0],
+            ['brown', 1],
+            ['red', 2],
+            ['orange', 3],
+            ['yellow', 4],
+            ['green', 5],
+            ['blue', 6],
+            ['violet', 7],
+            ['grey', 8],
+            ['white', 9]
+        ]
+    )
+
+    if (!colorMap.has(colors[0])) throw new Error('Invalid color');
+    if (!colorMap.has(colors[1])) throw new Error('Invalid color');
+
+    let first = colorMap.get(colors[0]) ?? 0;
+    let second = colorMap.get(colors[1]) ?? 0;
+
+    return first * 10 + second;
 }
