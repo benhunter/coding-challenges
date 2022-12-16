374ba46826afc07ea931b37f2152aa84771dbb0c
Ben Hunter <code@benhunter.me>
2022-12-16 00:17:46 -0600

    AOC 22 day 6

--- AdventOfCode/2022/aoc22/src/main/kotlin/Main.kt
+++ AdventOfCode/2022/aoc22/src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
-const val DEBUG = false
+const val DEBUG = true
 
 fun main(args: Array<String>) {
-    day1()
-    day2()
-    day3()
-    day4()
-    day5()
+//    day1()
+//    day2()
+//    day3()
+//    day4()
+//    day5()
+    day6()
 }
---
+++ AdventOfCode/2022/aoc22/src/main/kotlin/day6.kt
@@ -0,0 +1,53 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+fun day6() {
+    println("day 6")
+
+    val filename = "6-input.txt"
+//    val filename = "6-test.txt"
+    val text = getTextFromResource(filename).trim()
+    val lines = text.split("\n")
+    debugln(lines.toString())
+
+
+    var part1 = 0
+    lines.forEach { line ->
+        debug("$line ")
+
+        for (i in 4 until line.length) {
+            debug("$i ")
+
+            if (isUniqueChars(line.slice(i - 4 until i), 4)) {
+                debugln("")
+                part1 = i
+                break
+            }
+        }
+        println("part 1 $part1") // 1275
+    }
+
+
+    debugln("part 2")
+    var part2 = 0
+    val distinctPart2 = 14
+    lines.forEach { line ->
+        debug("$line ")
+
+        for (i in distinctPart2 until line.length) {
+            debug("$i ")
+
+            if (isUniqueChars(line.slice(i - distinctPart2 until i), distinctPart2)) {
+                debugln("")
+                part2 = i
+                break
+            }
+        }
+        println("part 2 $part2") // 3605
+    }
+}
+
+fun isUniqueChars(string: String, size: Int): Boolean {
+    return string.toSet().size == size
+}
