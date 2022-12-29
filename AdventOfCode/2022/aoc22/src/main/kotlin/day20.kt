cab3c3e8872a929380059d087d11dca48355de7f
Ben Hunter <code@benhunter.me>
2022-12-29 17:15:26 -0600

    AOC 22 day 20 WIP

--- AdventOfCode/2022/aoc22/src/main/kotlin/day20.kt
+++ AdventOfCode/2022/aoc22/src/main/kotlin/day20.kt
@@ -1,34 +1,87 @@
 fun day20() {
+
+//    val list = List(7) { it }.toMutableList()
+//    list.forEach { debugln(it) }
+//
+//    list.removeAt(2)
+//    list.add(4, 2)
+//    list.add(7, 7)
+
+
     val day = 20
     println("day $day")
     val startTime = System.nanoTime()
 
-//    val filename = "$day-input.txt"
-    val filename = "$day-test.txt"
+    val filename = "$day-input.txt"
+//    val filename = "$day-test.txt"
+//    val filename = "$day-test2.txt"
     val text = getTextFromResource(filename).trim()
     val lines = text.split("\n").map { it.toInt() }
-    debugln(lines)
+//    debugln(lines)
 
     val decrypted = lines.toMutableList()
 
+    val answer = getTextFromResource("20-test-answer.txt").trim().split("\n")
+//    val answer = getTextFromResource("20-test2-answer.txt").trim().split("\n")
+        .map { line -> line.split(",").map { it.trim().toInt() } }
+
     var part1 = 0
-    lines.forEachIndexed() { index, line ->
-        debug("index $index. line $line. ")
+    lines.forEachIndexed { index, line ->
+        debug("$line. ")
 
         val currentPosition = decrypted.indexOf(line)
         debug("currentPosition $currentPosition. ")
-        var newPosition = (currentPosition + line) % lines.size
-        if (newPosition < 0) newPosition += lines.size - 1
-        debug("newPosition $newPosition. ")
-        decrypted.add(newPosition + 1, line)
-        decrypted.remove(line)
+
+        var newPosition: Int
+        if (line > 0) {
+            newPosition = currentPosition + line
+
+            // wrap
+            if (newPosition > (lines.size - 1)) {
+                newPosition %= lines.size - 1
+                if (newPosition == 0) {
+                    newPosition = lines.size - 1
+                }
+            }
+            debug("newPosition $newPosition. ")
+        } else if (line < 0) {
+            newPosition = currentPosition + line
+
+            // wrap
+            if (newPosition < 1) {
+                newPosition %= lines.size - 1
+                newPosition += lines.size - 1
+                if (newPosition == 0) {
+                    newPosition = lines.size - 1
+                }
+            }
+            debug("newPosition $newPosition. ")
+        } else {
+            // line == 0
+            newPosition = currentPosition
+        }
+
+        if (newPosition != currentPosition) {
+            decrypted.remove(line)
+            decrypted.add(newPosition, line)
+        }
 
         debug("decrypted $decrypted. ")
-
+//        debug("answer: ${answer[index]} ")
+//        val match = answer[index] == decrypted
+//        debug("match: $match ")
+//        if (!match) throw Exception("failed to match answer")
         debugln()
     }
     debugln()
+    val posn0 = decrypted.indexOf(0)
+    part1 += decrypted[(posn0 + 1000) % decrypted.size]
+    part1 += decrypted[(posn0 + 2000) % decrypted.size]
+    part1 += decrypted[(posn0 + 3000) % decrypted.size]
     println("part 1 $part1")
+    // wrong:
+    // -5067
+    // 8722 too low
 
     val timeSeconds = elapsedTimeInSecondsSince(startTime)
     println("part 1 time: $timeSeconds seconds")
