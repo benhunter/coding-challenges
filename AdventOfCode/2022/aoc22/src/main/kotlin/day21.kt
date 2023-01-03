417902c120a5bac1f38cf6ab3f45972a83100f61
Ben Hunter <code@benhunter.me>
2023-01-02 21:12:24 -0600

    AOC 22 day 21 WIP

--- AdventOfCode/2022/aoc22/src/main/kotlin/day21.kt
+++ AdventOfCode/2022/aoc22/src/main/kotlin/day21.kt
@@ -1,3 +1,5 @@
+import kotlin.reflect.typeOf
+
 class Day21 {
     init {
         val day = 21
@@ -42,7 +44,40 @@ class Day21 {
         val operations = getOperations(input)
         val humn = operations["humn"]
         debugln("$humn")
-        return calc("root", operations)
+        val graph = buildGraph("root", operations)
+        debugln(graph)
+
+        prettyprint(graph)
+        // do things to graph
+        solve(graph)
+        return 0
+    }
+
+    data class NodeDepth(val node: OperationOrValue, val depth: Int)
+
+    private fun prettyprint(graph: OperationOrValue) {
+        val depth = 0
+        val nodesToVisit = mutableListOf(NodeDepth(graph, depth))
+        while (nodesToVisit.isNotEmpty()) {
+            val node = nodesToVisit.removeFirst()
+            print("${node.depth} ${node.node.name} ")
+
+            if (node.node is Operation) {
+                nodesToVisit.add(NodeDepth(node.node.left, node.depth + 1))
+                nodesToVisit.add(NodeDepth(node.node.right, node.depth + 1))
+                println()
+                continue
+            }
+
+            if (node.node is Value) {
+                println(node.node.value)
+            }
+        }
+    }
+
+    private fun getOperations(input: List<String>) = input.associate {
+        val tokens = it.split(":")
+        tokens[0] to tokens[1].trim()
     }
 
     private fun calc(monkey: String, operations: Map<String, String>): Long {
@@ -52,16 +87,43 @@ class Day21 {
         val right = calc(operation[2], operations)
         val result = when (operation[1]) {
             "+" -> left + right
-            "/" -> left / right
-            "*" -> left * right
             "-" -> left - right
+            "*" -> left * right
+            "/" -> left / right
             else -> TODO("Not yet implemented operator ${operation[1]}")
         }
         return result
     }
 
-    private fun getOperations(input: List<String>) = input.associate {
-        val tokens = it.split(":")
-        tokens[0] to tokens[1].trim()
+    open class OperationOrValue(open val name: String)
+    data class Value(override val name: String, val value: Long) : OperationOrValue(name)
+    enum class Operations { add, subtract, multiply, divide }
+
+    data class Operation(
+        override val name: String,
+        val operation: Operations,
+        val left: OperationOrValue,
+        val right: OperationOrValue
+    ) :
+        OperationOrValue(name)
+
+    private fun buildGraph(monkey: String, operations: Map<String, String>): OperationOrValue {
+        val tokens = operations[monkey]?.split(" ") ?: throw RuntimeException("Monkey $monkey not found")
+        if (tokens.size == 1) return Value(monkey, tokens[0].toLong())
+        val left = buildGraph(tokens[0], operations)
+        val right = buildGraph(tokens[2], operations)
+        val operation = when (tokens[1]) {
+            "+" -> Operations.add
+            "-" -> Operations.subtract
+            "/" -> Operations.divide
+            "*" -> Operations.multiply
+            else -> TODO("Not yet implemented operator ${tokens[1]}")
+        }
+        return Operation(monkey, operation, left, right)
     }
+
+    private fun solve(monkey: OperationOrValue) {
+
+    }
+
 }
