27cf10a046622659d266742a15a4ab17a880b331
Ben Hunter <code@benhunter.me>
2023-12-18 22:28:30 -0600

    Add just run day command. Add day-17 starter

---
+++ AdventOfCode/2023/day-17/benches/criterion.rs
@@ -0,0 +1,16 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use day_17::{solve_part1, solve_part2};
+
+pub fn day_17(c: &mut Criterion) {
+    let input = include_str!("../input.txt");
+    let mut group = c.benchmark_group("day-17");
+    group.bench_function("part1", |b| b.iter(|| solve_part1(black_box(input))));
+    group.bench_function("part2", |b| b.iter(|| solve_part2(black_box(input))));
+    group.bench_function("both", |b| b.iter(|| {
+        solve_part1(black_box(input));
+        solve_part2(black_box(input));
+    }));
+}
+
+criterion_group!(benches, day_17);
+criterion_main!(benches);
---
+++ AdventOfCode/2023/day-17/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    // let input = include_str!("../test.txt");
+
+    let result = day_17::solve_part1(input);
+    println!("✅ part1: {}", result);
+
+    // let result = day_17::solve_part2(input);
+    // println!("✅ part2: {}", result);
+}
