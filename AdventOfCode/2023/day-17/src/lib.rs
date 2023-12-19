821755eec3c283294b52b963d930f1398456f98b
Ben Hunter <code@benhunter.me>
2023-12-19 17:54:09 -0600

    Dijkstra's algorithm - bug with the 3 in a row rule

--- AdventOfCode/2023/day-17/src/lib.rs
+++ AdventOfCode/2023/day-17/src/lib.rs
@@ -1,41 +1,200 @@
 use crate::Direction::*;
+use crate::Distance::{Infinity, Value};
 
-pub fn solve_part1(input: &str) -> i32 {
+pub fn solve_part1(input: &str) -> usize {
     let mut city = parse(input);
     let start = Coord { x: 0, y: 0 };
     let bound = Coord { x: city.layout[0].len(), y: city.layout.len() };
     let destination = Coord { x: bound.x - 1, y: bound.y - 1 };
 
     // let mut paths_queue = vec![vec![start]];
-    let mut paths_queue = vec![Path(vec![start])];
+    // let mut paths_queue = vec![Path(vec![start])];
 
-    while let Some(mut current_path) = paths_queue.pop() {
-        println!("paths_queue len: {}", paths_queue.len());
-        let last_block = current_path.0.last().unwrap();
+    let mut distances = vec![vec![Infinity; bound.x]; bound.y];
+    distances[start.y][start.x] = Value(0);
+    let mut visited = vec![vec![false; bound.x]; bound.y];
+    let mut previous: Vec<Vec<(Option<Coord>, Option<Direction>)>> = vec![vec![(None, None); bound.x]; bound.y];
 
-        if last_block == &destination {
-            println!("path arrived at destination");
-            city.visualize(&current_path);
-            // dbg!(current_path);
-            continue
-        }
+    loop {
+        // if visited.iter().all(|blocks| !blocks.contains(&false)) {
+        //     break;
+        // }
 
-        // path will never cross itself (or loop)
-        for direction in Direction::iter() {
-            let mut path = current_path.clone();
-            // go 1,2,3 distance in all direction
-            // dbg!(direction.clone());
-
-            if let Some(next) = last_block.go(&direction, &bound, &path.0) {
-                path.0.push(next);
-                paths_queue.push(path.clone());
+        let mut current = None;
+        for y in 0..bound.y {
+            for x in 0..bound.x {
+                if !visited[y][x] && distances[y][x] != Infinity {
+                    current = Some(Coord::new(x, y));
+                    break;
+                }
             }
-            // dbg!(paths_queue);
+            if current.is_some() {
+                break;
+            }
         }
-    }
-    // dbg!(paths_queue);
+        if current.is_none() {
+            break;
+        }
 
-    todo!()
+        let current = current.unwrap();
+        println!("current: {:?}", current);
+
+        for direction in Direction::iter() {
+            if let Some(next) = current.go(&direction, &bound) {
+                println!("current: {:?}, next: {:?}, direction: {:?}", current, next, direction);
+
+                if current == Coord::new(4, 0) {
+                    visualize_previous(&previous, &city);
+                    visualize_distances(&distances);
+                    let debug_path = build_path(&start, bound, &current, &previous);
+                    visualize_path(&debug_path, &city);
+                    println!("🚫🐞");
+                }
+                // if current == Coord::new(4, 1) && direction == Up {
+                //     println!("debug");
+                // }
+
+                // TODO only consider next if the line is 3 or less
+                // track path, check last 3 for same line
+                // whatever direction we are going, look opposite
+                let prev1 = &previous[current.y][current.x];
+                if prev1.0.is_some() {
+                    let prev2 = &previous[prev1.0.unwrap().y][prev1.0.unwrap().x];
+                    if prev2.0.is_some() {
+                        let prev3 = &previous[prev2.0.unwrap().y][prev1.0.unwrap().x];
+                        if prev3.0.is_some() {
+                            if direction == *prev1.1.as_ref().unwrap() &&
+                                prev1.1.as_ref().unwrap() == prev2.1.as_ref().unwrap() &&
+                                prev2.1.as_ref().unwrap() == prev3.1.as_ref().unwrap() {
+                                println!("can't go more than 3 in the same direction");
+                                continue;
+                            }
+                        }
+                    }
+                }
+
+                // consider unvisited neighbors
+                // if !visited[next.y][next.x] { // TODO bug here - current: Coord { x: 4, y: 1 }, next: Coord { x: 4, y: 0 }, direction: Up
+                match distances[current.y][current.x] {
+                    Value(current_value) => {
+                        let new_dist = current_value + city.layout[next.y][next.x] as usize;
+
+                        match distances[next.y][next.x] {
+                            Infinity => {
+                                distances[next.y][next.x] = Value(new_dist);
+                                println!("set x: {}, y: {}, distance: {}", next.x, next.y, new_dist);
+                                previous[next.y][next.x] = (Some(current), Some(direction));
+                            }
+
+                            Value(dist) => {
+                                if new_dist < dist {
+                                    distances[next.y][next.x] = Value(new_dist);
+                                    println!("updated x: {}, y: {}, distance: {}", next.x, next.y, new_dist);
+                                    previous[next.y][next.x] = (Some(current), Some(direction));
+                                }
+                            }
+                        }
+                    }
+
+                    Infinity => {
+                        panic!("must have current_value")
+                    }
+                }
+                // }
+            }
+        }
+
+        visited[current.y][current.x] = true;
+        println!("set visited x: {}, y: {}", current.x, current.y);
+    }
+
+    let best_previous = build_path(&start, bound, &destination, &mut previous);
+
+    visualize_distances(&distances);
+    visualize_path(&best_previous, &city);
+    visualize_best_previous(&best_previous, &city);
+    visualize_previous(&previous, &city);
+    let Value(d) = distances[bound.y - 1][bound.x - 1] else { panic!("impossible 🧐") };
+    d
+}
+
+fn build_path<'a>(to: &Coord, bound: Coord, from: &Coord, previous: &'a Vec<Vec<(Option<Coord>, Option<Direction>)>>) -> Vec<Vec<&'a(Option<Coord>, Option<Direction>)>> {
+    let mut path: Vec<Vec<&(Option<Coord>, Option<Direction>)>> = vec![vec![&(None, None); bound.x]; bound.y];
+    let mut current = *from;
+    while current != *to {
+        path[current.y][current.x] = &previous[current.y][current.x];
+        let previous_coord = &previous[current.y][current.x].0.clone();
+        current = previous_coord.unwrap();
+    }
+    path
+}
+
+fn visualize_distances(distances: &Vec<Vec<Distance>>) {
+    for y in 0..distances.len() {
+        for x in 0..distances[0].len() {
+            match distances[y][x] {
+                Infinity => print!(". "),
+                Value(v) => {
+                    print!("{:>3} ", v);
+                }
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+fn visualize_path(previous: &Vec<Vec<&(Option<Coord>, Option<Direction>)>>, city: &City) {
+    for y in 0..previous.len() {
+        for x in 0..previous[0].len() {
+            match &previous[y][x].1 {
+                Some(direction) => match direction {
+                    Up => print!("^"),
+                    Down => print!("v"),
+                    Left => print!("<"),
+                    Right => print!(">"),
+                }
+                None => {
+                    // print!(".")
+                    print!("{}", city.layout[y][x]);
+                }
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+fn visualize_best_previous(previous: &Vec<Vec<&(Option<Coord>, Option<Direction>)>>, city: &City) {
+    for y in 0..previous.len() {
+        for x in 0..previous[0].len() {
+            match &previous[y][x].0 {
+                Some(coord) => print!("({:2},{:2})", coord.x, coord.y),
+                None => print!("       "),
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+fn visualize_previous(previous: &Vec<Vec<(Option<Coord>, Option<Direction>)>>, city: &City) {
+    for y in 0..previous.len() {
+        for x in 0..previous[0].len() {
+            match &previous[y][x].0 {
+                Some(coord) => print!("({:2},{:2})", coord.x, coord.y),
+                None => print!("       "),
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Distance {
+    Infinity,
+    Value(usize),
 }
 
 pub fn solve_part2(input: &str) -> i32 {
@@ -44,7 +203,7 @@ pub fn solve_part2(input: &str) -> i32 {
 
 #[derive(Debug, PartialEq, Clone)]
 struct City {
-    layout: Vec<Vec<char>>,
+    layout: Vec<Vec<u8>>,
 }
 
 impl City {
@@ -67,7 +226,7 @@ impl City {
 #[derive(Debug, Clone)]
 struct Path(Vec<Coord>);
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Copy)]
 struct Coord {
     x: usize,
     y: usize,
@@ -78,27 +237,18 @@ impl Coord {
         Coord { x, y }
     }
 
-    pub(crate) fn go(&self, direction: &Direction, bound: &Coord, path: &Vec<Coord>) -> Option<Coord> {
-        let next = match direction {
+    pub(crate) fn go(&self, direction: &Direction, bound: &Coord) -> Option<Coord> {
+        match direction {
             Up if self.y > 0 => Some(Coord { x: self.x, y: self.y - 1 }),
             Down if self.y < bound.y - 1 => Some(Coord { x: self.x, y: self.y + 1 }),
             Left if self.x > 0 => Some(Coord { x: self.x - 1, y: self.y }),
             Right if self.x < bound.x - 1 => Some(Coord { x: self.x + 1, y: self.y }),
             _ => None,
-        };
-
-        if let Some(next) = next {
-            return if path.contains(&next) {
-                None
-            } else {
-                Some(next)
-            };
         }
-        None
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 enum Direction {
     Up = 0,
     Down = 1,
@@ -115,12 +265,23 @@ impl Direction {
             Direction::Up,
         ].into_iter()
     }
+
+    pub(crate) fn opposite(&self) -> Direction {
+        match self {
+            Up => Down,
+            Down => Up,
+            Left => Right,
+            Right => Left,
+        }
+    }
 }
 
 fn parse(input: &str) -> City {
     let layout = input
         .lines()
-        .map(|line| line.chars().collect())
+        .map(|line| line.chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect())
         .collect();
     City { layout }
 }
@@ -135,14 +296,15 @@ mod tests {
         let actual = parse(input);
         assert_eq!(actual.layout.len(), 13);
         assert_eq!(actual.layout[0].len(), 13);
+        assert_eq!(actual.layout[0][0], 2);
     }
 
     #[test]
     fn test_go() {
         let input = "11\n11";
         let city = parse(input);
-        let path = Path(vec![Coord::new(1, 0)]);
-        let go = Coord::new(0, 0).go(&Right, &Coord::new(2, 2), &path.0);
+        // let path = Path(vec![Coord::new(1, 0)]);
+        let go = Coord::new(0, 0).go(&Left, &Coord::new(2, 2));
         assert_eq!(go, None);
     }
 
