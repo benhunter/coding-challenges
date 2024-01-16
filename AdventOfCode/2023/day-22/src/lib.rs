83cd630dffba3e86525e9740d34c210806d76ba7
Ben Hunter <code@benhunter.me>
2024-01-16 01:19:40 -0600

    Bricks fall

--- AdventOfCode/2023/day-22/src/lib.rs
+++ AdventOfCode/2023/day-22/src/lib.rs
@@ -21,7 +21,7 @@ y_min = 0
 */
 
 pub fn solve_part1(input: &str) -> Result<i32, String> {
-    let stack = parse(input)?;
+    let mut stack = parse(input)?;
     stack.check_invalid();
     stack.bricks.iter().for_each(|brick| {
         // println!("{:?}", brick);
@@ -45,72 +45,101 @@ struct Stack {
 }
 
 impl Stack {
-    pub(crate) fn fall(&self) {
+    pub(crate) fn fall(&mut self) {
         let z_max = self.bricks.iter()
             .map(|brick| {
                 brick.position_end.2
             })
             .max().unwrap();
 
-        (2..=z_max).for_each(|z| {
-            if z > 3 {
-                panic!("debug")
+        let mut loop_count = 0;
+        loop {
+            println!("Loop count: {}", loop_count);
+            let mut did_any_bricks_move = false;
+            let mut moved_bricks = vec![];
+
+            (2..=z_max).for_each(|z| {
+                // if z > 5 {
+                //     panic!("debug")
+                // }
+                let updated_bricks = self.bricks.iter()
+                    // find bricks at z
+                    .filter(|brick| {
+                        brick.position_start.2 == z
+                    })
+                    // .inspect(|&brick| { dbg!(brick); })
+                    .filter(|brick| {
+                        // check if there is a brick under it
+                        let bricks_under = self.bricks.iter()
+                            // skip self
+                            .filter(|&other| brick.id != other.id)
+                            .filter(|&lower_brick| {
+                                if !(lower_brick.position_start.2 <= (z - 1)
+                                    && lower_brick.position_end.2 >= (z - 1)) {
+                                    return false;
+                                }
+
+                                // brick corners
+                                // nw - ne
+                                // sw - se
+                                // let nw = (brick.position_start.0, brick.position_end.1);
+                                let brick_ne = (brick.position_end.0, brick.position_end.1);
+                                let brick_sw = (brick.position_start.0, brick.position_start.1);
+                                // let se = (brick.position_end.0, brick.position_start.1);
+
+                                if (brick.position_end.0 < lower_brick.position_start.0 || brick.position_start.0 > lower_brick.position_end.0) {
+                                    return false;
+                                }
+
+                                if (brick.position_end.1 < lower_brick.position_start.1 || brick.position_start.1 > lower_brick.position_end.1) {
+                                    return false;
+                                }
+
+                                true
+                            }).collect::<Vec<&Brick>>();
+                        bricks_under.is_empty()
+                    })
+                    .map(|brick| {
+
+                        // move brick down
+                        Brick {
+                            position_start: (brick.position_start.0, brick.position_start.1, brick.position_start.2 - 1),
+                            position_end: (brick.position_end.0, brick.position_end.1, brick.position_end.2 - 1),
+                            id: brick.id,
+                        }
+                    })
+                    .collect::<Vec<Brick>>();
+
+                // if not, move it down
+                // update self.bricks
+                self.bricks.iter_mut()
+                    .for_each(|brick| {
+                        let b = brick.clone();
+                        updated_bricks.iter()
+                            .filter(|update| {
+                                update.id == b.id //&&
+                            })
+                            .for_each(|update| {
+                                *brick = update.clone();
+                                moved_bricks.push(brick.id);
+                            });
+                    });
+
+                self.render_xz();
+
+                if !updated_bricks.is_empty() {
+                    println!("Bricks moved");
+                    did_any_bricks_move = true;
+                }
+            });
+
+            if !did_any_bricks_move {
+                println!("No bricks moved");
+                break;
             }
-            let updated_bricks = self.bricks.iter()
-                // find bricks at z
-                .filter(|brick| {
-                    brick.position_start.2 <= z && brick.position_end.2 >= z
-                })
-                .inspect(|&brick| { dbg!(brick); })
-                .filter(|brick| {
-                    // check if there is a brick under it
-                    let bricks_under = self.bricks.iter()
-                        // skip self
-                        .filter(|&other| brick.id != other.id)
-                        .filter(|&lower_brick| {
-                            if !(lower_brick.position_start.2 <= (z - 1)
-                                && lower_brick.position_end.2 >= (z - 1)) {
-                                return false;
-                            }
 
-                            // brick corners
-                            // nw - ne
-                            // sw - se
-                            // let nw = (brick.position_start.0, brick.position_end.1);
-                            let brick_ne = (brick.position_end.0, brick.position_end.1);
-                            let brick_sw = (brick.position_start.0, brick.position_start.1);
-                            // let se = (brick.position_end.0, brick.position_start.1);
-
-                            if (brick.position_end.0 < lower_brick.position_start.0 || brick.position_start.0 > lower_brick.position_end.0) {
-                                return false;
-                            }
-
-                            if (brick.position_end.1 < lower_brick.position_start.1 || brick.position_start.1 > lower_brick.position_end.1) {
-                                return false;
-                            }
-
-                            true
-                        }).collect::<Vec<&Brick>>();
-                    dbg!(bricks_under.clone());
-                    bricks_under.is_empty()
-                })
-                .map(|brick| {
-
-                    // move brick down
-                    Brick {
-                        position_start: (brick.position_start.0, brick.position_start.1, brick.position_start.2 - 1),
-                        position_end: (brick.position_end.0, brick.position_end.1, brick.position_end.2 - 1),
-                        id: brick.id,
-                    }
-                })
-                .collect::<Vec<Brick>>();
-            // if not, move it down
-            dbg!(updated_bricks);
-
-            // TODO update self.bricks
-        });
-
-        todo!()
+            loop_count += 1;
+        }
     }
 }
 
@@ -153,20 +182,27 @@ impl Stack {
         // render rows from z_max to 0
         (1..=z_max).rev().for_each(|z| {
             (0..=x_max).for_each(|x| {
-                let closest_y = self.bricks.iter()
+                let bricks_y = self.bricks.iter()
                     .filter(|brick| {
                         brick.position_start.0 <= x && brick.position_end.0 >= x
                             && brick.position_start.2 <= z && brick.position_end.2 >= z
                     })
+                    .collect::<Vec<&Brick>>();
+                let closest_y = bricks_y.iter()
                     .map(|brick| brick.id)
                     .max().unwrap_or('.');
                 print!("{}", closest_y);
-                output.push(closest_y);
+
+                if bricks_y.len() > 1 {
+                    output.push('?');
+                } else {
+                    output.push(closest_y);
+                }
             });
 
             print!(" {}", z);
             output.push_str(&format!(" {}", z));
-            if z == z_max / 2 + 1 {
+            if z == (z_max + 1) / 2 {
                 print!(" z");
                 output.push_str(" z");
             }
@@ -366,7 +402,7 @@ AAA 1
     #[test]
     fn test_fall() -> Result<(), String> {
         let input = include_str!("../test.txt");
-        let stack = parse(input)?;
+        let mut stack = parse(input)?;
         stack.fall();
         let actual = stack.render_xz();
         let expected = " x
