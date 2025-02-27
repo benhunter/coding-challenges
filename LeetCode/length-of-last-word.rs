45d3da20b0ba3e9ac1dd1ee359c3c67428d80818
Ben Hunter <code@benhunter.me>
2025-02-26 23:35:52 -0600

    LeetCode length-of-last-word faster!

--- LeetCode/length-of-last-word.rs
+++ LeetCode/length-of-last-word.rs
@@ -47,7 +47,8 @@
 
 impl Solution {
     pub fn length_of_last_word(s: String) -> i32 {
-        s.split_whitespace().last().unwrap().len() as i32
+        s.split_whitespace().last()
+            .map_or(0, |word| word.len() as i32)
     }
 }
 // @leetup=code
