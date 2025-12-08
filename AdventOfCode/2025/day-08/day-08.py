b53f9ce6ce02c2b5784cabd5a21f35f9a467a4d0
Ben Hunter <code@benhunter.me>
2025-12-08 07:25:40 +0000

    WIP AOC 2025 day 8

--- AdventOfCode/2025/day-08/day-08.py
+++ AdventOfCode/2025/day-08/day-08.py
@@ -25,7 +25,36 @@ def part1(junctions):
         # print(a+b, dists[a+b])
     # print(dists.items())
     sorted_d = sorted(dists.items(), key=lambda x: x[1])
-    pprint(sorted_d)
+    # pprint(sorted_d)
+
+    connections = []
+    for pair in sorted_d[:10]:
+        connections.append([pair])
+    connections = [c[0][0].split() for c in connections]
+    pprint(connections)
+    print()
+
+    circuit_count = 0
+    circuits = {}
+    for c in connections:
+        # print(c)
+        if c[0] in circuits or c[1] in circuits:
+            # print(f"One is in! {circuits[c[0]]}")
+            if circuits.get(c[0]) is not None:
+                circuits[c[1]] = circuits[c[0]]
+                # print(circuits[c[1]])
+            elif circuits.get(c[1]) is not None:
+                circuits[c[0]] = circuits[c[1]]
+        else:
+            circuits[c[0]] = circuit_count
+            circuits[c[1]] = circuit_count
+            circuit_count += 1
+
+        # pprint(circuits)
+        # print()
+
+    print()
+    pprint(circuits)
 
 
 p1_test = part1(get("day-08.test.in"))
